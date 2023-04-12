5e807d163a96b6b74667674fa4f996569f47b34b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-13 01:56:07 +0300

    add input to Header

--- src/assets/components/ui/Header/Header.tsx
+++ src/assets/components/ui/Header/Header.tsx
@@ -11,6 +11,35 @@ const Header: FC<HeaderProps> = ({}) => {
     <header className={cn(styles.appHeader)}>
       <div className={cn(styles.navbar)}>
         <Logo />
+
+        <div className={cn(styles.input)}>
+          <input type={'text'} placeholder={'Поиск...'} />
+
+          <svg
+            width='35'
+            height='38'
+            viewBox='0 0 35 38'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <circle
+              cx='22.5'
+              cy='12.5'
+              r='11'
+              fill='#FDFDFD'
+              stroke='black'
+              strokeWidth='3'
+            />
+            <line
+              x1='1.90569'
+              y1='36.9741'
+              x2='16.9057'
+              y2='20.9741'
+              stroke='black'
+              strokeWidth='3'
+            />
+          </svg>
+        </div>
       </div>
 
       <div className={cn(styles.bottomText)}>
