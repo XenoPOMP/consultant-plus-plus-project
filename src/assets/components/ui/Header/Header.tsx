ba91c564ebdb717511dbc2cda637fc4838fa96c7
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-13 00:26:08 +0300

    update Header

--- src/assets/components/ui/Header/Header.tsx
+++ src/assets/components/ui/Header/Header.tsx
@@ -3,11 +3,32 @@ import { FC } from 'react';
 import styles from './Header.module.scss';
 import { HeaderProps } from './Header.props';
 import Logo from '@ui/Logo/Logo';
+import ProgressiveImage from '@ui/ProgressiveImage/ProgressiveImage';
+import saulImg from '@media/images/soul_consultant.png';
 
 const Header: FC<HeaderProps> = ({}) => {
   return (
     <header className={cn(styles.appHeader)}>
-      <Logo />
+      <div className={cn(styles.navbar)}>
+        <Logo />
+      </div>
+
+      <div className={cn(styles.bottomText)}>
+        <div className={cn(styles.text)}>
+          Дорогие друзья, вы когда-нибудь задумывались о том, что у вас есть
+          права?
+        </div>
+
+        <div className={cn(styles.saulPlaceholder)}>
+          <ProgressiveImage
+            loaderColorScheme={{
+              backgroundColor: 'transparent',
+              loaderColor: 'black',
+            }}
+            src={saulImg}
+          />
+        </div>
+      </div>
     </header>
   );
 };
--- src/assets/components/ui/Logo/Logo.tsx
+++ src/assets/components/ui/Logo/Logo.tsx
@@ -2,9 +2,14 @@ import cn from 'classnames';
 import { FC } from 'react';
 import styles from './Logo.module.scss';
 import { LogoProps } from './Logo.props';
+import { Link } from 'react-router-dom';
 
 const Logo: FC<LogoProps> = ({}) => {
-  return <div className={cn(styles.logo)}>СолГудман</div>;
+  return (
+    <Link to={'/'} className={cn(styles.logo)}>
+      СолГудман
+    </Link>
+  );
 };
 
 export default Logo;
